f3ea5bbd66c6bb5c1d80187a791a440ead22950c
rodneysgraphics <2831078+rodneysgraphics@users.noreply.github.com>
2020-02-02 16:59:59 -0500

    file upload
    logo randomizer upload

---
+++ Logo_randomizer2.py
@@ -0,0 +1,38 @@
+from sys import argv
+import sys
+
+
+from random import choice
+from common_noun import *
+
+
+i=0
+
+foo = ['a', 'b', 'c', 'd', 'e']
+goo = ['f', 'g', 'h', 'h', 'i']
+hoo = ['j', 'k', 'l', 'm', 'n']
+
+abc = ['A','B','C','D','E','F','G','H','I','J',
+'K','L','M','N','O','P','Q','R','S','T','U','V',
+'W','X','Y','Z']
+
+things = ['airplane','apple','bee','bird','bus','bolt','cut','cow','dice','email','fly','gear','hula','industry','juice',
+'kangaroo','knife','letter','mail','next','orange','pottery','queen','roof','screw','toilet','umbrella','vacume',
+'wing','xpress','yack','zebra']
+
+
+
+while i < 30:
+	i = i + 1
+	#print "[",
+	print choice(abc) , 
+	print choice(abc) ,
+	#print "]" ,
+	print choice(common_n) ,
+	print choice(common_n)
+
+
+
+
+
+
